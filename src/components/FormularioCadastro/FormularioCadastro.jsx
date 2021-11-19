8b749eda3d359c892c3526ca3a0b703a20ce4ffb
Lucas-Mororo <lucasmmororo@gmail.com>
2021-11-19 10:06:57 -0300

    ...

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 import "./estilo.css";
-
 class FormularioCadastro extends Component {
-  constructor(props) {
+
+  constructor(props){
     super(props);
-    this.titulo = "";
-    this.Nota = "";
+    this.titulo ="";
+    this.texto ="";
   }
 
-  _handleMudancaTitulo(evento) {
+  _handleMudancaTitulo(evento){
     evento.stopPropagation();
     this.titulo = evento.target.value;
-    //console.log(this.titulo);
   }
 
-  _handleMudancaNota(evento) {
+  _handleMudancaTexto(evento){
     evento.stopPropagation();
-    this.Nota = evento.target.value;
-    //console.log(this.Nota)
+    this.texto = evento.target.value;
   }
 
-  _criarNota(evento) {
+  _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.Nota);
-    //console.log(`Uma nova nota foi criada ` + this.titulo + " "+ this.Nota);
+    this.props.criarNota(this.titulo, this.texto);
+    
   }
 
   render() {
     return (
-      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
@@ -40,7 +40,7 @@ class FormularioCadastro extends Component {
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
-          onChange={this._handleMudancaNota.bind(this)}
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
