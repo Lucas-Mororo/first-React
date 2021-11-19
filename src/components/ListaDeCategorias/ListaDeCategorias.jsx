97f385c288dcfa3dba2cb4d2ecb480a1a050055c
Lucas-Mororo <lucasmmororo@gmail.com>
2021-11-19 13:07:19 -0300

    adicionando estilo as categorias

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import "./estilo.css";
 
 class ListaDeCategorias extends React.Component {
   render() {
     return (
-      <section>
-        <ul>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
         </ul>
-        <input type="text" />
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar Categoria"
+        />
       </section>
     );
   }
