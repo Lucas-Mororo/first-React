10481ab48be9729b862c0af01bb6365a3283cede
Lucas-Mororo <lucasmmororo@gmail.com>
2021-11-23 08:26:57 -0300

    React: Ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -4,56 +4,31 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
-
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      notas: [],
-      categorias: [],
-    };
-  }
-
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
-    const novoArrayNotas = [...this.state.notas, novaNota];
-    const novoEstado = {
-      notas: novoArrayNotas,
-    };
-    this.setState(novoEstado);
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    const novoEstado = {...this.state, categorias:novoArrayCategorias};
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index) {
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index, 1);
-    console.log("deletar");
-    this.setState({
-      notas: arrayNotas,
-    });
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+    
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro 
-          criarNota={this.criarNota.bind(this)}
-          categorias={this.state.categorias}  
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
         />
         <main className="conteudo-principal">
           <ListaDeCategorias
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias}
           />
           <ListaDeNotas
-            notas={this.state.notas}
-            apagarNota={this.deletarNota.bind(this)}
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
           />
         </main>
       </section>
@@ -61,4 +36,5 @@ class App extends Component {
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
